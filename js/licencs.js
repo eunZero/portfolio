81390e67427216db2cda43681304c287d5efe4a1
eunZero <64447945+eunZero@users.noreply.github.com>
2021-03-14 13:28:47 +0900

    Add files via upload

---
+++ js/licencs.js
@@ -0,0 +1,57 @@
+$(function(){
+  $(".license00").click(function(){
+    $(".big_license00").css("display","block");
+  });
+    $(".close").click(function(){
+    $(".big_license00").css("display","none");
+  });
+    
+    $(".license01").click(function(){
+    $(".big_license01").css("display","block");
+  });
+    $(".close").click(function(){
+    $(".big_license01").css("display","none");
+  });
+    
+    $(".license02").click(function(){
+    $(".big_license02").css("display","block");
+  });
+    $(".close").click(function(){
+    $(".big_license02").css("display","none");
+  });
+    
+    $(".license03").click(function(){
+    $(".big_license03").css("display","block");
+  });
+    $(".close").click(function(){
+    $(".big_license03").css("display","none");
+  });
+    
+    $(".license04").click(function(){
+    $(".big_license04").css("display","block");
+  });
+    $(".close").click(function(){
+    $(".big_license04").css("display","none");
+  });
+    
+    $(".license05").click(function(){
+    $(".big_license05").css("display","block");
+  });
+    $(".close").click(function(){
+    $(".big_license05").css("display","none");
+  });
+    
+    $(".license06").click(function(){
+    $(".big_license06").css("display","block");
+  });
+    $(".close").click(function(){
+    $(".big_license06").css("display","none");
+  });
+    
+    $(".license07").click(function(){
+    $(".big_license07").css("display","block");
+  });
+    $(".close").click(function(){
+    $(".big_license07").css("display","none");
+  });
+})
